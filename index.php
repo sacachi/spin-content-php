dfbc51c676617a7fa338cccec00b90e0aed83825
sacachi <39644554+sacachi@users.noreply.github.com>
2018-09-27 23:31:04 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?PHP
+
+class Spintax
+{
+    public function process($text)
+    {
+        return preg_replace_callback(
+            '/\{(((?>[^\{\}]+)|(?R))*)\}/x',
+            array($this, 'replace'),
+            $text
+        );
+    }
+    public function replace($text)
+    {
+        $text = $this->process($text[1]);
+        $parts = explode('|', $text);
+        return $parts[array_rand($parts)];
+    }
+}
+/* EXAMPLE USAGE */
+$spintax = new Spintax();
+$string = '{Hello|Howdy|Hola} to you, {Mr.|Mrs.|Ms.} {Smith|Williams|Davis}!';
+echo $spintax->process($string);
+/* NESTED SPINNING EXAMPLE */
+echo $spintax->process('{Hello|Howdy|Hola} to you, {Mr.|Mrs.|Ms.} {{Jason|Malina|Sara}|Williams|Davis}');
